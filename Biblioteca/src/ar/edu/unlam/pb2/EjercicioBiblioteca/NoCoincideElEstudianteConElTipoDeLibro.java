abb06ea22e91136f51b009382956ff0316d7e178
TomiiVaz <88063172+TomiiVaz@users.noreply.github.com>
2021-12-03 19:04:14 -0300

    Add files via upload

---
+++ Biblioteca/src/ar/edu/unlam/pb2/EjercicioBiblioteca/NoCoincideElEstudianteConElTipoDeLibro.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.pb2.EjercicioBiblioteca;
+
+public class NoCoincideElEstudianteConElTipoDeLibro extends Exception {
+
+	public NoCoincideElEstudianteConElTipoDeLibro(String mensaje) {
+		super(mensaje);
+	}
+}
